6169cdc0783b3cce7db479e871477c41cd82355b
duffield44 <duffieldbenjamin44@gmail.com>
2015-02-21 19:51:38 +0800

    Create Question Manager as an object

--- js/app.js
+++ js/app.js
@@ -3,44 +3,52 @@ $(document).ready(function(){
 	/*---Click Start button---*/
 	$('.start').on('click', function(){
 		$('.intro').fadeOut(500);
+		qManager.showQuestion();
 		$('.play-area').delay(500).fadeIn(500);
 	})
 
-
-
-var question1 = {
-	question: "In the 2003/04 season, which team won the Premier League?",
-	pic: ["images/arsenal.png", "images/man-u.png", "images/swansea.png", "images/chelsea.png"],
-	answers: ["Arsenal", "Manchester United", "Swansea City", "Chelsea"],
-	correctAnswer: "Arsenal"
-};
-var question2 = 0
-var question3 = 0
-var question4 = 0
-var question5 = 0
-
-var questionList = [question1, question2, question3, question4, question5];
-
-var currentNum = 0;
-var currentQuestion = questionList[currentNum];
-var questionPic = currentQuestion.pic;
-var questionAnswers = currentQuestion.answers;
-var currentAnswer = currentQuestion.correctAnswer;
-
-$('.content').find('.cur-quest-num').text(currentNum +1);
-$('.question').text(currentQuestion.question);
-$('#answer1').find('img').attr("src", questionPic[0]);
-$('#answer2').find('img').attr("src", questionPic[1]);
-$('#answer3').find('img').attr("src", questionPic[2]);
-$('#answer4').find('img').attr("src", questionPic[3]);
-$('#answer1 .answer-text').text(questionAnswers[0]);
-$('#answer2 .answer-text').text(questionAnswers[1]);
-$('#answer3 .answer-text').text(questionAnswers[2]);
-$('#answer4 .answer-text').text(questionAnswers[3]);
-
-$('.unselected').click(function() {
+	/*---Add Selected class when user clicks answer---*/
+	$('.unselected').click(function() {
 	$('.selected').removeClass('selected').addClass('unselected');
 	$(this).removeClass('unselected').addClass('selected');
-	
-})
+	})
+
+	/*---Questions---*/
+	var question1 = {
+		question: "In the 2003/04 season, which team won the Premier League?",
+		pic: ["images/arsenal.png", "images/man-u.png", "images/swansea.png", "images/chelsea.png"],
+		answers: ["Arsenal", "Manchester United", "Swansea City", "Chelsea"],
+		correctAnswer: "Arsenal"
+	};
+	var question2 = 0;
+	var question3 = 0;
+	var question4 = 0;
+	var question5 = 0;
+
+	var list = [question1, question2, question3, question4, question5];
+
+
+	/*---Question Manager--*/
+	var qManager = {
+
+		currentNum: 0,
+		showQuestion : function(){
+
+			var currentQuestion = list[this.currentNum];
+			var questionPic = currentQuestion.pic;
+			var questionAnswers = currentQuestion.answers;
+
+			/*---Add content to question from current question in list array---*/	
+			$('.content').find('.cur-quest-num').text(this.currentNum +1);
+			$('.question').text(currentQuestion.question);
+			$('#answer1').find('img').attr("src", questionPic[0]);
+			$('#answer2').find('img').attr("src", questionPic[1]);
+			$('#answer3').find('img').attr("src", questionPic[2]);
+			$('#answer4').find('img').attr("src", questionPic[3]);
+			$('#answer1 .answer-text').text(questionAnswers[0]);
+			$('#answer2 .answer-text').text(questionAnswers[1]);
+			$('#answer3 .answer-text').text(questionAnswers[2]);
+			$('#answer4 .answer-text').text(questionAnswers[3]);
+		}
+	}
 });
