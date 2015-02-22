4be6e6eb753f3ae92461301252807418b1314cae
duffield44 <duffieldbenjamin44@gmail.com>
2015-02-22 09:38:18 +0800

    Add question2 content

--- js/app.js
+++ js/app.js
@@ -44,11 +44,11 @@ $(document).ready(function(){
 	};
 	var question2 = {
 		question: "In the 2008/09 season, who won the PFA PLayer of the Year?",
-		pic: ["images/man-u.png", "images/arsenal.png", "images/chelsea.png", "images/arsenal.png"],
-		answers: ["Arsenal", "Manchester United", "Swansea City", "Chelsea"],
-		correctAnswer: "Arsenal",
-		answerPic: "images/arsenal.png",
-		ansDescription: "The 2003–04 FA Premier League was the twelfth season of the Premier League. In the end, Arsenal went through the season without a single defeat – the first team ever to do so in a 38 game league season and the second team overall (the first was Preston North End in 1889, 115 years earlier, during a 22 game league season) and were crowned champions once more, at the expense of Chelsea, who had spent heavily throughout the season."
+		pic: ["images/cristiano-ronaldo.png", "images/luke-chadwick.png", "images/ryan-giggs.png", "images/steven-gerrard.png"],
+		answers: ["Cristiano Ronaldo", "Luke Chadwick", "Ryan Giggs", "Steven Gerrard"],
+		correctAnswer: "Ryan Giggs",
+		answerPic: "images/ryan-giggs.png",
+		ansDescription: "Ryan Joseph Giggs is the most decorated player in Welsh and English football history. He was known for his tireless running, ball possession and ability to create goalscoring opportunities for those around him. He holds the Manchester United club record for competitive appearances. During his time at United, he won 13 Premier League winner's medals, four FA Cup winner's medals, three League Cup winner's medals and two Champions League winner's medals. He was the first player in history to win two consecutive PFA Young Player of the Year awards (1992 and 1993), though he did not win the PFA Player of the Year award until 2009."
 	};
 	var question3 = 0;
 	var question4 = 0;
