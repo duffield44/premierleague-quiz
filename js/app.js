11aa369ba959d266288be9a5f1f5a9de6f87bf67
duffield44 <duffieldbenjamin44@gmail.com>
2015-02-22 14:12:17 +0800

    Add content for question3-5

--- js/app.js
+++ js/app.js
@@ -29,6 +29,8 @@ $(document).ready(function(){
 		qManager.currentNum++;
 		qManager.showQuestion();
 		console.log(qManager.currentNum);
+		$('.selected').removeClass('selected').addClass('unselected');
+		$('#answer').empty();
 		$('.answer-area').fadeOut(500);
 		$('.play-area').delay(500).fadeIn(500);
 	})
@@ -50,9 +52,30 @@ $(document).ready(function(){
 		answerPic: "images/ryan-giggs.png",
 		ansDescription: "Ryan Joseph Giggs is the most decorated player in Welsh and English football history. He was known for his tireless running, ball possession and ability to create goalscoring opportunities for those around him. He holds the Manchester United club record for competitive appearances. During his time at United, he won 13 Premier League winner's medals, four FA Cup winner's medals, three League Cup winner's medals and two Champions League winner's medals. He was the first player in history to win two consecutive PFA Young Player of the Year awards (1992 and 1993), though he did not win the PFA Player of the Year award until 2009."
 	};
-	var question3 = 0;
-	var question4 = 0;
-	var question5 = 0;
+	var question3 = {
+		question: "Which player leads the scoring charts with 260 premiership goals?",
+		pic: ["images/teddy-sheringham.png", "images/robin-van-persie.png", "images/ryan-giggs.png", "images/alan-shearer.png"],
+		answers: ["Teddy Sheringham", "Robin van Persie", "Ryan Giggs", "Alan Shearer"],
+		correctAnswer: "Alan Shearer",
+		answerPic: "images/alan-shearer.png",
+		ansDescription: "Ryan Joseph Giggs is the most decorated player in Welsh and English football history. He was known for his tireless running, ball possession and ability to create goalscoring opportunities for those around him. He holds the Manchester United club record for competitive appearances. During his time at United, he won 13 Premier League winner's medals, four FA Cup winner's medals, three League Cup winner's medals and two Champions League winner's medals. He was the first player in history to win two consecutive PFA Young Player of the Year awards (1992 and 1993), though he did not win the PFA Player of the Year award until 2009."
+	};
+	var question4 = {
+		question: "Who was the first ever African player in the premiership?",
+		pic: ["images/tony-yeboah.png", "images/peter-ndlovu.png", "images/jj-okocha.png", "images/nwankwo-kanu.png"],
+		answers: ["Tony Yeboah", "Peter Ndlovu", "Jay Jay Okocha", "Nwankwo Kanu"],
+		correctAnswer: "Peter Ndlovu",
+		answerPic: "images/peter-ndlovu.png",
+		ansDescription: "Peter Ndlovu was a Zimbabwean professional footballer and is currently Manager at Mamelodi Sundowns F.C. (South Africa). He played as a striker from 1988 until 2011, and spent time playing in England, where he appeared in the Premier League for Coventry City and in the Football League for Birmingham City, Huddersfield Town and Sheffield United. A veteran of 100 international games over a 15-year period, Ndlovu is the all-time leading scorer for Zimbabwe with 38 goals."
+	};
+	var question5 = {
+		question: "Who scored the Premiership's first hatrick?",
+		pic: ["images/teddy-sheringham.png", "images/ian-rush.png", "images/les-ferdinand.png", "images/eric-cantona.png"],
+		answers: ["Teddy Sheringham", "Ian Rush", "Les Ferdinand", "Eric Cantona"],
+		correctAnswer: "Eric Cantona",
+		answerPic: "images/eric-cantona.png",
+		ansDescription: "Éric Daniel Pierre Cantona played for Auxerre, Martigues, Marseille, Bordeaux, Montpellier, Nîmes and Leeds United before ending his career at Manchester United where he won four Premier League titles in five years and two League and FA Cup Doubles. Cantona is often regarded as having played a key role in the revival of Manchester United as a footballing force in the 1990s and he enjoys iconic status at the club. An inaugural inductee into the English Football Hall of Fame in 2002, the museum states: 'The enigmatic Frenchman was one of the Premier League's most talented, controversial players'. He scored the Premiership's first ever hatrick for Leeds United with a 5–0 win over Tottenham Hotspur."
+	};
 
 	var list = [question1, question2, question3, question4, question5];
 
@@ -61,6 +84,7 @@ $(document).ready(function(){
 	var qManager = {
 
 		currentNum: 0,
+		
 		showQuestion : function(){
 
 			var currentQuestion = list[this.currentNum];
@@ -89,6 +113,7 @@ $(document).ready(function(){
 			/*---Show correct answer content---*/	
 			$('.answer-pic').find('img').attr("src", picture);
 			$('#correct-answer').text(answer);
+			$('.description p').remove();
 			$('.description').append("<p>" + description +"</p>");
 
 			/*---Tell user if he is Correct or Incorrect---*/	
